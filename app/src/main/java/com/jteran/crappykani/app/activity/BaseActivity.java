dafbb0dac8b6db9272ea08e289bfcba1473924ba
jonathannerat <jonathan.nerat@gmail.com>
2019-04-14 17:09:02 -0300

    add base activity

--- app/src/main/java/com/jteran/crappykani/app/activity/BaseActivity.java
+++ app/src/main/java/com/jteran/crappykani/app/activity/BaseActivity.java
@@ -1,7 +1,31 @@
 package com.jteran.crappykani.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 
-public class BaseActivity extends AppCompatActivity {
+import com.hannesdorfmann.mosby3.mvp.MvpActivity;
+import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
+import com.hannesdorfmann.mosby3.mvp.MvpView;
 
+public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>> extends MvpActivity<V, P> {
+
+    /**
+     * Specifies the contentView layout file for the activity
+     *
+     * @return the layout resource to be used
+     */
+    protected abstract @LayoutRes
+    int layout();
+
+    /**
+     * View bindings that should be done right after specifying the contentView
+     */
+    protected abstract void viewBindings();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout());
+        viewBindings();
+    }
 }
